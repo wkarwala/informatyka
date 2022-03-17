0fe9fc2d673f3adc32e7e40d1958189e910052a9
wkarwala <99956360+wkarwala@users.noreply.github.com>
2022-03-17 20:30:12 +0100

    Add files via upload

---
+++ Algorytm liczb pierwszych Karwala 3d.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    int p = 2;
+    int lp = 0;
+    bool o;
+    cout << "Podaj ile liczb pierwszych chcesz wyświetlić: ";
+    cin >> n;
+    cout << "Liczby pierwsze: ";
+    while (lp < n)
+    {
+        o = true;
+        for (int i = 2; i < p; i++)
+        {
+            if (p % i == 0)
+            {
+                o = false;
+                break;
+            }
+        }
+
+        if (o)
+        {
+            cout << p << " ";
+            lp = lp+1;
+        }
+        p = p+1;
+    }
+}

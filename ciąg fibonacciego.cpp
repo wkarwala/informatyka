624402ff2b46b6945dabece3dd31c4d1122243e5
wkarwala <99956360+wkarwala@users.noreply.github.com>
2022-05-01 09:59:51 +0200

    Add files via upload

---
+++ ciąg fibonacciego.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+	int n = 0;
+	int f = 0;
+	int f0 = 0;
+	int f1 = 1;
+
+	cout << "Ile wyrazow ciagu wypisac: ";
+	
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		if (i > 1) {
+			f = f1 + f0;
+			f0 = f1;
+			f1 = f;
+		}
+		else {
+			f = 1;
+		}
+		cout << f << endl;
+
+	}
+
